8ecf19dfe31d4ecb253fd27cbcb6969a6010ed98
Asmit Sen <susmita.sen1130@gmail.com>
2025-04-18 00:11:05 +0530

    minor changes

--- src/pages/AddHackathonPage.jsx
+++ src/pages/AddHackathonPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import AddHackathonForm from '../components/hackathon/AddHackathonForm';
 
